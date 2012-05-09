c44fc0e44a70e095d718779a5a1445d998926138
Dan Belwood <dbelwood@modelmetrics.com>
2012-05-09 12:37:13 -0500

    Messaging updates

--- travail.js
+++ travail.js
@@ -1,5 +1,8 @@
-var URL = require('url'),
-	redis = require('redis');
+var URL = 	require('url'),
+	util = 	require('util'),
+	events = require('events'),
+	redis = require('redis'),
+	amqp = 	require('amqp');
 
 var Travail = {};
 Travail.Configuration = function(url) {
@@ -40,12 +43,44 @@ Travail.Configuration.prototype.setJson = function(key, object, callback) {
 	this.set(key, JSON.stringify(object), callback);
 }
 
+Travail.Messaging = function(configuration) {
+	var self = this;
+	configuration.get('Messaging:AMQP_URL', function(err, reply) {
+		self.connection = amqp.createConnection({url: reply});
+		self.connection.on('ready', function(err, reply) {
+			self.emit('ready');	
+		});
+	});
+	events.EventEmitter.call(this);
+}
+util.inherits(Travail.Messaging, events.EventEmitter);
+module.exports = Travail;
+Travail.Messaging.prototype.publish = function(routingKey, value) {
+	this.connection.publish(routingKey, value);
+}
+Travail.Messaging.prototype.publishJson = function(routingKey, object) {
+	this.publish(routingKey, JSON.stringify(object));
+}
+Travail.Messaging.prototype.subscribe = function(queue, subscriptionOptions, callback) {
+	this.connection.queue(queue, function(queue) {
+		queue.on('basicConsumeOk', function() {
+			console.log('subscribed.');
+		});
+		if (callback === null) {
+			if (subscriptionOptions instanceof Function) {
+				callback = subscriptionOptions;
+				queue.subscribe(callback);
+			}
+			else {
+				console.error("subscriptionOptions is optional, but a callback function is required.");
+			}
+		}
+		else {
+			queue.subscribe(subscriptionOptions, callback);
+		}
+	});
+}
+
 Travail.Logging = function(configuration) {
 
 }
-
-Travail.Messaging = function(configuration) {
-
-}
-
-module.exports = Travail;
